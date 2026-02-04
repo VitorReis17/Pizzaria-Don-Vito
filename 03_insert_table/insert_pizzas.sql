b7f36933a3a60235f6f5a8db1e526d06bcc83d64
VitorReis17 <vitorodev@gmail.com>
2026-02-04 19:46:23 -0300

    Adicionando a pasta de insert com as tabelas populadas

---
+++ 03_insert_table/insert_pizzas.sql
@@ -0,0 +1,11 @@
+use pizzaria;
+
+INSERT INTO pizzas (nome, preco) VALUES
+('Calabresa', 45.00),
+('Mussarela', 42.00),
+('Portuguesa', 48.00),
+('Frango com Catupiry', 50.00),
+('Quatro Queijos', 52.00),
+('Pepperoni', 55.00),
+('Marguerita', 44.00),
+('Vegetariana', 49.00);
